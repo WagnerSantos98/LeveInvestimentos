3407fd6927b4b65336fce8aa24a06abdeacba832
Wagner Santos <wagner.jesus98@outlook.com>
2026-04-30 00:29:03 -0300

    fix: alteração no serviço do usuário, para funcionalidades de edição e exclusão

--- LeveInvestimentos.Infrastructure/Services/UserService.cs
+++ LeveInvestimentos.Infrastructure/Services/UserService.cs
@@ -67,5 +67,15 @@ namespace LeveInvestimentos.Infrastructure.Services
             _context.Users.Update(user);
             await _context.SaveChangesAsync();
         }
+
+        public async Task DeleteAsync(Guid id)
+        {
+            var user = await _context.Users.FindAsync(id);
+            if (user != null)
+            {
+                _context.Users.Remove(user);
+                await _context.SaveChangesAsync();
+            }
+        }
     }
 }
