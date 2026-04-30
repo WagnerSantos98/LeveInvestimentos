edcfe733a82c278442590b737da1eed4eca9e92c
Wagner Santos <wagner.jesus98@outlook.com>
2026-04-30 00:48:41 -0300

    feat: implementação para exclusão de usuário

--- LeveInvestimentos.Web/Pages/Users/Index.cshtml.cs
+++ LeveInvestimentos.Web/Pages/Users/Index.cshtml.cs
@@ -17,13 +17,32 @@ namespace LeveInvestimentos.Web.Pages.Users
         {
             _userService = userService;
         }
-
+        
         public IEnumerable<User> Users { get; set; }
 
         public async Task<IActionResult> OnGetAsync()
         {
             Users = await _userService.GetAllAsync();
             return Page();
+        }
+
+        public async Task<IActionResult> onPostDeleteAsync(System.Guid id)
+        {
+            if(!User.HasClaim(System.Security.Claims.ClaimTypes.Role, "Manager"))
+            {
+                TempData["ErrorMessage"] = "Você não tem permissão para excluir usuários.";
+                return RedirectToPage("/Users/Index");
+            }
+            try
+            {
+                await _userService.DeleteAsync(id);
+                TempData["SuccessMessage"] = "Usuário excluído com sucesso!";
+            }
+            catch(System.Exception ex)
+            {
+                TempData["ErrorMessage"] = ex.Message;
+            }
+            return RedirectToPage("/Users/Index");
         }
     }
 }
